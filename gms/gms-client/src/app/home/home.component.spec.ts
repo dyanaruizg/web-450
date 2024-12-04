dacc704930c913cf57e1d5357e8fc3f027f15b49
Diana Ruiz Garcia <dyanar_04@hotmail.com>
2024-12-03 20:03:18 -0700

    feat(): Building the frontend of the Garden Management

--- gms/gms-client/src/app/home/home.component.spec.ts
+++ gms/gms-client/src/app/home/home.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HomeComponent } from './home.component';
-import { environment } from '../../environments/environment';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
